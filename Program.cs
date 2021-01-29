9114651ba1e7aa2f318801bea7a02ee3e4e52bd7
Özer Kutlu ÜNAL <ozer@unal-group.com>
2021-01-30 00:53:51 +0300

    16 Ocak Ders Sonu

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            Console.WriteLine(kategoriEtiketi);
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
